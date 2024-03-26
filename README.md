558c4aaad368d2db62137d3510c0f894030244f2
Tyler K. <tylerkruer1@gmail.com>
2024-03-26 10:02:44 -0700

    Sadly I think I need to use the Java API over Go API, more documentation + more complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# picklejar
-Simple KV Store with data validation via pkl
+# Pickle Jar
+
+Simple KV Store with data validation via pkl. Picklejar is a simple wrapper around pkl that provides type safety and validation for KV stores. **Picklejar is not production ready, and is ongoing active development**.
+
+## Usage
+
+## Examples
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,1 +1,48 @@
 package client
+
+import (
+	"log"
+	"net"
+
+	"github.com/tkruer/picklejar/pkg/session"
+)
+
+type Client struct {
+	Port string
+}
+
+func (c *Client) Start() {
+	listener, err := net.Listen("tcp", c.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Listening on tcp://0.0.0.0:%s", c.Port)
+
+	for {
+		conn, err := listener.Accept()
+		log.Println("New connection", conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go session.StartSession(conn)
+	}
+}
+
+func (c *Client) Stop() {
+	listener, err := net.Listen("tcp", c.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Listening on tcp://0.0.0.0:%s", c.Port)
+
+	for {
+		conn, err := listener.Accept()
+		log.Println("New connection", conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// TODO: Implement a StopSession safe shutdown
+		// go session.StopSession(conn)
+	}
+}
